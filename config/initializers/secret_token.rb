f1b4e5e2e5cbff48a301e1c6515d41de6ab5edc4
Max <msprior@gmail.com>
2010-09-23 16:21:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuckery::Application.config.secret_token = '6d899f0eb771a4b7af7779ed68444aa27b20aab2a8320cd0fffbf76d977036a4d3e0b083d102fe44dfd18d4ad1db93c584ad443fc766978f8dbde8ad7319cb1c'
